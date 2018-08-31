b78dad9ab225c2c05c7155d30e757379d073b6cd
keshavyerra <keshav.yerra@gmail.com>
2018-08-31 15:02:01 -0400

    worked on _menu.scss

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,11 +1,11 @@
-//select DOM elements
+// Select DOM Items
 const menuBtn = document.querySelector(".menu-btn");
 const menu = document.querySelector(".menu");
 const menuNav = document.querySelector(".menu-nav");
 const menuBranding = document.querySelector(".menu-branding");
 const navItems = document.querySelectorAll(".nav-item");
 
-//set intial state of menu
+// Set Initial State Of Menu
 let showMenu = false;
 
 menuBtn.addEventListener("click", toggleMenu);
@@ -17,6 +17,8 @@ function toggleMenu() {
     menuNav.classList.add("show");
     menuBranding.classList.add("show");
     navItems.forEach(item => item.classList.add("show"));
+
+    // Set Menu State
     showMenu = true;
   } else {
     menuBtn.classList.remove("close");
@@ -24,6 +26,8 @@ function toggleMenu() {
     menuNav.classList.remove("show");
     menuBranding.classList.remove("show");
     navItems.forEach(item => item.classList.remove("show"));
+
+    // Set Menu State
     showMenu = false;
   }
 }
